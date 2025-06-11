d236cc570c55e0cd00e03a2200fce434ea170850
gnimian <160290644+gnimian@users.noreply.github.com>
2025-06-11 12:17:24 -0700

    Update 95_pyfltk_intro.py

--- CS2/pyfltk/95_pyfltk_intro.py
+++ CS2/pyfltk/95_pyfltk_intro.py
@@ -2,10 +2,20 @@ from fltk import *
 
 def foo(wid):
     print('you clicked me, ouch')
+    wid.color(FL_GREEN)
+    #but.color()
 
-w1 = Fl_Window(400, 300, 'this is my first window')
-but = Fl_Button(0, 0, 100, 30, 'click me')
-but.callback(foo)
+pic=Fl_JPEG_Image("1.jpeg")
+pic = pic.copy(300,300)
+
+w1=Fl_Window(400, 300, 'this is my first window')
+#but = Fl_Button(0, 0, 100, 30, 'Click me')
+but2 = Fl_Button(0, 100, 100, 30, 'me too')
+but2.image(pic)
+#but.callback(foo)
+#but2.callback(foo)
 w1.show()
 
 Fl.run()
+
+
