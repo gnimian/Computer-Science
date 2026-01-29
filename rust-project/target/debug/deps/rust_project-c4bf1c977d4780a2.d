db9ade0a9124a865eaa5039acbb0678fae9f3197
Naiming <whybcznm@gmail.com>
2026-01-29 15:04:05 -0800

    New

--- rust-project/target/debug/deps/rust_project-c4bf1c977d4780a2.d
+++
@@ -1,5 +0,0 @@
-/Users/naimingzheng/Code/rust-project/target/debug/deps/rust_project-c4bf1c977d4780a2.d: src/main.rs
-
-/Users/naimingzheng/Code/rust-project/target/debug/deps/rust_project-c4bf1c977d4780a2: src/main.rs
-
-src/main.rs:
--- rust-project/target/debug/rust-project.d
+++
@@ -1,1 +0,0 @@
-/Users/naimingzheng/Code/rust-project/target/debug/rust-project: /Users/naimingzheng/Code/rust-project/src/main.rs
