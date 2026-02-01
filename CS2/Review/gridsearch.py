613a15834e7382e296a8b6e69fec8fcb37d384a4
Naiming <whybcznm@gmail.com>
2026-01-31 20:20:42 -0800

    new

--- CS2/Review/gridsearch.py
+++ CS2/Review/gridsearch.py
@@ -3,7 +3,7 @@ L=[]
 for i in range(4):
     l=[]
     num=0
-    while len(l)<=2:
+    while len(l)<3:
         a=random.randrange(0,3)
         if a == 0:
             num+=1
@@ -16,8 +16,31 @@ for i in range(len(L)):
     for j in range(len(L[i])):
         print(f'{L[i][j]} ', end='')
     print()
-
+print()
 for i in range(len(L)):
     for j in range(len(L[i])):
         if L[i][j]==0:
             print(j,i)
+
+#Teacher Solution
+'''
+L=[]
+for r in range(4):
+    t=[]
+    while True:
+        num=random.randrange(3)
+        if num==0 and 0 in t:
+            continue
+        t.append(num)
+        break  
+    L.append(t)
+for r in range(4):
+    for c in range(3):
+        print(f'{L[r][c]} ', end='')
+    print()
+print()
+for r in range(4):
+    for c in range(3):
+        if L[r][c]==0:
+            print(r,c)
+'''
